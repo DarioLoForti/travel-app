12cd70cb7c3c63311d58039ca31f18fa3344dcc4
Dario Lo Forti <lofortidario@gmail.com>
2024-08-28 10:20:18 +0200

    Remove unused image field in stageController

--- Backend/controllers/stageController.js
+++ Backend/controllers/stageController.js
@@ -14,7 +14,6 @@ const store = async (req, res) => {
     const data = {
       title,
       description,
-      image,
       rating: parseInt(rating),
       notes,
       dayId: parseInt(dayId),
